70e04d49ac8fe5acf5f87e0a42ddf96d27f547b9
David <murphd40@gmail.com>
2018-09-02 14:37:36 +0100

    committing changes

--- src/main/java/com/example/demo/leaseservice/dao/model/LeaseEntity.java
+++ src/main/java/com/example/demo/leaseservice/dao/model/LeaseEntity.java
@@ -4,11 +4,33 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+/**
+ * A lease is a contract which entitles a tenant / group of tenants to live in a property for a fixed monthly fee
+ * <p>
+ * Properties:
+ * - start date
+ * - duration (some leases are indefinite)
+ * - monthly fee
+ * <p>
+ * Entities involved:
+ * - exactly one property
+ * - at least one tenant
+ */
 @Data
 @Document
 public class LeaseEntity {
 
     @Id
     private String id;
+
+    private String propertyId;
+
+    private String tenantGroupId;
+
+    private long startDate;
+
+    private int durationInDays;
+
+    private int monthlyFee; // currency?
 
 }
